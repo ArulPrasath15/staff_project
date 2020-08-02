8a06a4ce1b06b2752c6061055215ac13e6a65162
r-deepti <deeptiravi01@gmail.com>
2020-08-02 12:43:49 +0530

    CO-PO Mapping

---
+++ Final.php
@@ -0,0 +1,260 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CO PO MAPPING</title>
+    <link rel="icon" type="image/png" href="./images/logo.png">
+     <link rel="stylesheet" href="./assets/Fomantic/dist/semantic.min.css" type="text/css"/> 
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+  />
+</head>
+<style>
+    body
+    {
+        margin:0;
+        padding:0;
+        font-family: sans-serif;
+        background:url("./Images/bg.jpg") ;
+        background-size: cover;
+    }
+    .topic
+    {
+        font-family:"Times New Roman", Times, serif;
+        color:black;
+        padding:50px;
+    }
+    .card
+    {
+        position:relative;
+        top:50%;
+        left: 50%;
+        transform:translate(-650px,-300px);
+        width:1300px;
+        background:white;
+        box-sizing: border-box;
+        box-shadow: 0 15px 25px rgba(0,0,0,.5);
+        border-radius: 10px;
+    }
+    table
+    {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+    td, th 
+    {
+        border: 1px solid #dddddd;
+        text-align: center;
+        padding: 18px;
+    }
+</style>
+<body>
+<!-- Navbar start -->
+    <!-- <div class="preloader"><body><div class="ui active dimmer" style="position: fixed;"><div class="ui massive active green elastic loader"></div></div></body></div> -->
+    <div class="ui tablet computer only padded grid">
+      <div class="ui borderless fluid  inverted menu" style="font-size:16px">
+        <a href="./index.php" class="active green item" style="font-size:20px">KEC Student +</a>
+        <!--  <a  class="right align item"  style="margin-left:1220px"   href="../Logout.php" style="font-size:20px"><i class="share square outline icon"></i>Logout</a>-->
+      </div>
+    </div>
+<!-- Navbar ended -->
+    <div class="card">
+    <div class="topic">
+        <h1 style="font-size:40px"><center>CO-PO MAPPING</center></h1>
+    </div>
+    <table>
+    <h2>&nbsp;ATTAINMENT OF COURSE OUTCOMES(COs)</h2>
+    <h2>&nbsp;Attainment through Cumulative Internal Examinations(CIE)</h2>
+    <br>
+    <tr>
+        <td rowspan="2" style="background: #dddddd">COs</td>
+        <th colspan="5">Assessment Methods</th>
+        <th rowspan="2">Attainment<br>level of CO</th>
+        <th rowspan="2">Mapped<br>PO</th>
+        <th rowspan="2">Mapped<br>PSO's</th>
+    </tr>
+    <tr>
+        <th>CA<br>Test-I</th>
+        <th>CA<br>Test-II</th>
+        <th>CA<br>Test-III</th>
+        <th>Assignment</th>
+        <th>Other<br>Assessments</th>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO1</td>
+        <td>2.67</td>
+        <td> </td>
+        <td> </td>
+        <td>4</td>
+        <td>5</td>
+        <td>3.89</td>
+        <td>PO=1,2,3</td>
+        <td>PSO=1,2</td>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO2</td>
+        <td>2.71</td>
+        <td>2</td>
+        <td> </td>
+        <td>4</td>
+        <td> </td>
+        <td>2.9</td>
+        <td>PO=1,2,3</td>
+        <td>PSO=1,2</td>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO3</td>
+        <td> </td>
+        <td>2</td>
+        <td>3.8</td>
+        <td>4</td>
+        <td>5</td>
+        <td>3.7</td>
+        <td>PO=1,2,3</td>
+        <td>PSO=1,2</td>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO4</td>
+        <td> </td>
+        <td> </td>
+        <td>1</td>
+        <td>4</td>
+        <td> </td>
+        <td>2.5</td>
+        <td>PO=1,2,3</td>
+        <td>PSO=1,2</td>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO5</td>
+        <td> </td>
+        <td> </td>
+        <td>1.8 </td>
+        <td>4</td>
+        <td> </td>
+        <td>2.9</td>
+        <td>PO=1</td>
+        <td>PSO=1</td>
+    </tr>
+    </table>
+    <br>
+    <table>
+    <h2>&nbsp;Attainment through Semester End Examination(SEE)<h2>
+    <tr>
+        <td style="background: #dddddd">COs</td>
+        <th>Attainment level of<br>CO</th>
+        <th>Mapped COs</th>
+        <th>Mapped PSOs</th>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO1</td>
+        <td>5</td>
+        <td>PO=1,2,3</td>
+        <td>PSO=1,2</td>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO2</td>
+        <td>5</td>
+        <td>PO=1,2,3</td>
+        <td>PSO=1,2</td>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO3</td>
+        <td>5</td>
+        <td>PO=1,2,3</td>
+        <td>PSO=1,2</td>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO4</td>
+        <td>5</td>
+        <td>PO=1,2,3</td>
+        <td>PSO=1,2</td>
+    </tr>
+    <tr>
+        <td style="background: #dddddd">CO5</td>
+        <td>5</td>
+        <td>PO=1,2</td>
+        <td>PSO=1</td>
+    </tr>
+    </table>
+    <table>
+    <br>
+    <h2>&nbsp;ATTAINMENT OF PROGRAM OUTCOMES(Pos)</h2>
+    <br>
+    <tr>
+        <th style="background: #dddddd">Pos</th>
+        <th colspan="12">POs</th>
+        <th colspan="2">PSOs</th>
+    </tr>
+    <tr>
+        <th> </th>
+        <th>1</th>
+        <th>2</th>
+        <th>3</th>
+        <th>4</th>
+        <th>5</th>
+        <th>6</th>
+        <th>7</th>
+        <th>8</th>
+        <th>9</th>
+        <th>10</th>
+        <th>11</th>
+        <th>12</th>
+        <th>1</th>
+        <th>2</th>
+    </tr>
+    <tr>
+        <th style="background: #dddddd">Attainment<br>level of PO<br>from CIE</th>
+        <td>3.18</td>
+        <td>3.18</td>
+        <td>3.25</td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td>3.18</td>
+        <td>3.18</td>
+    </tr>
+    <tr>
+        <th style="background: #dddddd">Attainment<br>level of PO<br>from SEE</th>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td>5</td>
+        <td>5</td>
+    </tr>
+    <tr>
+        <th style="background: #dddddd">Attainment<br>level of PO<br>(50% of CIE<br>50% of SEE)</th>
+        <td>4.1</td>
+        <td>4.1</td>
+        <td>4.13</td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+        <td>4.1</td>
+        <td>4.1</td>
+    </tr>
+    </table>
+    </div>
+</body>
